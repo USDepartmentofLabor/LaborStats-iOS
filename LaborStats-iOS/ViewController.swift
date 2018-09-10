3fde99ac6b911a7b93813d70fb4671c640dc9bd2
Hari Yerramsetty <yhvardhan@gmail.com>
2018-09-10 10:50:38 -0400

    Initial commit for version 2.0

---
+++ LaborStats-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LaborStats-iOS
+//
+//  Created by Hari Yerramsetty on 9/10/18.
+//  Copyright © 2018 Hari Yerramsetty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
